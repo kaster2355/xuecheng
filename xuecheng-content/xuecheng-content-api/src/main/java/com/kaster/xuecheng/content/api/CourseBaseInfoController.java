0fc029692d6dc49db7d902dc198868ff0d21288a
kaster <948560589@qq.com>
2023-07-08 14:06:21 +0800

    编写课程信息分页查询接口

--- xuecheng-content/xuecheng-content-api/src/main/java/com/kaster/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-content/xuecheng-content-api/src/main/java/com/kaster/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,8 +4,10 @@ import com.kaster.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.kaster.xuecheng.content.model.po.CourseBase;
 import com.kaster.xuecheng.base.model.PageParams;
 import com.kaster.xuecheng.base.model.PageResult;
+import com.kaster.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/course")
 public class CourseBaseInfoController {
 
+    @Autowired
+    private CourseBaseInfoService courseBaseInfoService;
+
     @ApiOperation("课程查询接口")
     @PostMapping("/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
-        return null;
+        return courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
     }
 }
