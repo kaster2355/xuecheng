f1671a13c14a5e6474b9f4163bac0d0cbb8d848c
kaster <948560589@qq.com>
2023-07-12 12:26:37 +0800

    网关模块建立与配置

---
+++ xuecheng-gateway/src/test/java/com/kaster/xuecheng/XuechengGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaster.xuecheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
