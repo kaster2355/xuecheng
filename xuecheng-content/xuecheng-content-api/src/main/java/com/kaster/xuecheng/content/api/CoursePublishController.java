ba8c84f4210271b5b573f08e7848e8aa881f9994
kaster <948560589@qq.com>
2023-07-18 16:42:12 +0800

    定义预览接口

---
+++ xuecheng-content/xuecheng-content-api/src/main/java/com/kaster/xuecheng/content/api/CoursePublishController.java
@@ -0,0 +1,24 @@
+package com.kaster.xuecheng.content.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 课程预览，发布
+ */
+
+@Controller
+public class CoursePublishController {
+
+    @GetMapping("/coursepreview/{courseId}")
+    public ModelAndView preview(@PathVariable("courseId") Long courseId){
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("model",null);
+        modelAndView.setViewName("course_template");
+        return modelAndView;
+    }
+
+}
