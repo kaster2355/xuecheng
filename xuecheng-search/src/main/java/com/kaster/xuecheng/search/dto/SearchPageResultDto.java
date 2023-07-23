262b9fef36d34f7aa46200a761f56d8baf4704e0
kaster <948560589@qq.com>
2023-07-23 23:57:31 +0800

    导入课程搜索工程

---
+++ xuecheng-search/src/main/java/com/kaster/xuecheng/search/dto/SearchPageResultDto.java
@@ -0,0 +1,22 @@
+package com.kaster.xuecheng.search.dto;
+
+import com.kaster.xuecheng.base.model.PageResult;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@ToString
+public class SearchPageResultDto<T> extends PageResult<T> {
+
+    //大分类列表
+    List<String> mtList;
+    //小分类列表
+    List<String> stList;
+
+    public SearchPageResultDto(List<T> items, long counts, long page, long pageSize) {
+        super(items, counts, page, pageSize);
+    }
+
+}
