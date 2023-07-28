4254bbc256d63c3eceecc146bdc5ad0b1f57a090
kaster <948560589@qq.com>
2023-07-28 16:01:21 +0800

    账号认证验证码验证处理

--- xuecheng-auth/src/main/java/com/kaster/xuecheng/AuthApplication.java
+++ xuecheng-auth/src/main/java/com/kaster/xuecheng/AuthApplication.java
@@ -2,8 +2,10 @@ package com.kaster.xuecheng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages={"com.kaster.xuecheng.*.feignclient"})
 public class AuthApplication {
 
     public static void main(String[] args) {
--- xuecheng-auth/src/main/java/com/kaster/xuecheng/ucenter/service/impl/PasswordAuthServiceImpl.java
+++ xuecheng-auth/src/main/java/com/kaster/xuecheng/ucenter/service/impl/PasswordAuthServiceImpl.java
@@ -1,11 +1,13 @@
 package com.kaster.xuecheng.ucenter.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.kaster.xuecheng.ucenter.feignclient.CheckCodeClient;
 import com.kaster.xuecheng.ucenter.mapper.XcUserMapper;
 import com.kaster.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.kaster.xuecheng.ucenter.model.dto.XcUserExt;
 import com.kaster.xuecheng.ucenter.model.po.XcUser;
 import com.kaster.xuecheng.ucenter.service.AuthService;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,8 +22,25 @@ public class PasswordAuthServiceImpl implements AuthService {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    CheckCodeClient checkCodeClient;
+
     @Override
     public XcUserExt execute(AuthParamsDto authParamsDto) {
+
+        // 远程调用验证码服务校验
+        String checkcode = authParamsDto.getCheckcode();
+        String checkcodekey = authParamsDto.getCheckcodekey();
+
+        if (Strings.isEmpty(checkcodekey) || Strings.isEmpty(checkcode)){
+            throw new RuntimeException("请输入验证码");
+        }
+
+        Boolean verify = checkCodeClient.verify(checkcodekey, checkcode);
+        if (verify == null || !verify){
+            throw new RuntimeException("验证码输入错误");
+        }
+
         //账号
         String username = authParamsDto.getUsername();
         XcUser user = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));
