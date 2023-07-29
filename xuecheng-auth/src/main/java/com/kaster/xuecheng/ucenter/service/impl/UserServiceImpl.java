3e7853b3948159c440436af98575bb2122c07ddd
kaster <948560589@qq.com>
2023-07-30 01:48:33 +0800

    课程列表查询授权

--- xuecheng-auth/src/main/java/com/kaster/xuecheng/ucenter/service/impl/UserServiceImpl.java
+++ xuecheng-auth/src/main/java/com/kaster/xuecheng/ucenter/service/impl/UserServiceImpl.java
@@ -1,11 +1,11 @@
 package com.kaster.xuecheng.ucenter.service.impl;
 
 import com.alibaba.fastjson.JSON;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.kaster.xuecheng.ucenter.mapper.XcMenuMapper;
 import com.kaster.xuecheng.ucenter.mapper.XcUserMapper;
 import com.kaster.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.kaster.xuecheng.ucenter.model.dto.XcUserExt;
-import com.kaster.xuecheng.ucenter.model.po.XcUser;
+import com.kaster.xuecheng.ucenter.model.po.XcMenu;
 import com.kaster.xuecheng.ucenter.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +15,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class UserServiceImpl implements UserDetailsService {
 
@@ -23,6 +26,9 @@ public class UserServiceImpl implements UserDetailsService {
 
     @Autowired
     ApplicationContext applicationContext;
+
+    @Autowired
+    XcMenuMapper xcMenuMapper;
 
 
     @Override
@@ -45,8 +51,14 @@ public class UserServiceImpl implements UserDetailsService {
     }
 
     public UserDetails getUserPrincipal(XcUserExt user) {
-        //用户权限,如果不加报Cannot pass a null GrantedAuthority collection
+
         String[] authorities = {"p1"};
+        //查询用户权限
+        List<XcMenu> xcMenus = xcMenuMapper.selectPermissionByUserId(user.getId());
+        if (xcMenus.size()>0){
+            authorities = xcMenus.stream().map(XcMenu::getCode).toArray(String[]::new);
+        }
+
         String password = user.getPassword();
         //为了安全在令牌中不放密码
         user.setPassword(null);
--- xuecheng-content/xuecheng-content-api/src/main/java/com/kaster/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-content/xuecheng-content-api/src/main/java/com/kaster/xuecheng/content/api/CourseBaseInfoController.java
@@ -12,6 +12,7 @@ import com.kaster.xuecheng.content.util.SecurityUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +27,7 @@ public class CourseBaseInfoController {
     private CourseBaseInfoService courseBaseInfoService;
 
     @ApiOperation("课程分页查询接口")
+    @PreAuthorize("hasAuthority('xc_teachmanager_course_list')")
     @PostMapping("/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
 
