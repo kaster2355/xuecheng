d28743bb62e2fd4f67ffc5a5968b1b93d4ea5e65
kaster <948560589@qq.com>
2023-07-31 00:59:00 +0800

    导入学习模块

---
+++ xuecheng-learning/xuecheng-learning-service/src/main/java/com/kaster/xuecheng/learning/mapper/XcChooseCourseMapper.java
@@ -0,0 +1,15 @@
+package com.kaster.xuecheng.learning.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kaster.xuecheng.learning.model.po.XcChooseCourse;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcChooseCourseMapper extends BaseMapper<XcChooseCourse> {
+
+}
---
+++ xuecheng-learning/xuecheng-learning-service/src/main/java/com/kaster/xuecheng/learning/mapper/XcCourseTablesMapper.java
@@ -0,0 +1,15 @@
+package com.kaster.xuecheng.learning.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kaster.xuecheng.learning.model.po.XcCourseTables;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcCourseTablesMapper extends BaseMapper<XcCourseTables> {
+
+}
