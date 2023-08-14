e1249205e0583cd2a2f991de87660d722efedb78
kaster <948560589@qq.com>
2023-08-14 20:11:26 +0800

    获取课程

--- xuecheng-content/xuecheng-content-api/src/main/java/com/kaster/xuecheng/content/api/CoursePublishController.java
+++ xuecheng-content/xuecheng-content-api/src/main/java/com/kaster/xuecheng/content/api/CoursePublishController.java
@@ -1,9 +1,13 @@
 package com.kaster.xuecheng.content.api;
 
+import com.alibaba.fastjson.JSON;
+import com.kaster.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.kaster.xuecheng.content.model.dto.CoursePreviewDto;
+import com.kaster.xuecheng.content.model.dto.TeachplanDto;
 import com.kaster.xuecheng.content.model.po.CoursePublish;
 import com.kaster.xuecheng.content.service.CoursePublishService;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +15,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * 课程预览，发布
@@ -35,15 +41,15 @@ public class CoursePublishController {
 
     @ResponseBody
     @PostMapping("/courseaudit/commit/{courseId}")
-    public void commitAudit(@PathVariable("courseId") Long courseId){
+    public void commitAudit(@PathVariable("courseId") Long courseId) {
         Long companyId = 1232141425L;
         coursePublishService.commitAudit(companyId, courseId);
     }
 
     @ApiOperation("课程发布")
     @ResponseBody
-    @PostMapping ("/coursepublish/{courseId}")
-    public void coursePublish(@PathVariable("courseId") Long courseId){
+    @PostMapping("/coursepublish/{courseId}")
+    public void coursePublish(@PathVariable("courseId") Long courseId) {
 
         Long companyId = 1232141425L;
 
@@ -57,4 +63,23 @@ public class CoursePublishController {
         return coursePublishService.getCoursePublish(courseId);
     }
 
+    @ApiOperation("获取课程发布信息")
+    @ResponseBody
+    @GetMapping("/course/whole/{courseId}")
+    public CoursePreviewDto getCoursePublish(@PathVariable("courseId") Long courseId) {
+        CoursePublish coursePublish = coursePublishService.getCoursePublish(courseId);
+        if (coursePublish == null) {
+            return new CoursePreviewDto();
+        }
+
+        //课程基本信息
+        CourseBaseInfoDto courseBase = new CourseBaseInfoDto();
+        BeanUtils.copyProperties(coursePublish, courseBase);
+        //课程计划
+        List<TeachplanDto> teachplans = JSON.parseArray(coursePublish.getTeachplan(), TeachplanDto.class);
+        CoursePreviewDto coursePreviewInfo = new CoursePreviewDto();
+        coursePreviewInfo.setCourseBase(courseBase);
+        coursePreviewInfo.setTeachplans(teachplans);
+        return coursePreviewInfo;
+    }
 }
